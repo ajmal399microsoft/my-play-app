4ce7f7c49d1707f5ab7e76bb2209578cb946ce76
Muhammed Ajmal IK <muhammedajmal.ik@cat.com>
2022-07-17 21:56:00 +0530

    make it better

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,10 @@
 import axios from "axios"
 
+// export default axios.create({
+//     baseURL:"http://localhost:5000"
+// })
+
+//Prod heroku
 export default axios.create({
-    baseURL:"http://localhost:5000"
+    baseURL:"https://my-play-app-ajm.herokuapp.com"
 })
